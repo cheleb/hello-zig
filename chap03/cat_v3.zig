d036029fbca13fba7f16f5c775157c834b7b5c02
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2026-09-13 11:19:02 +0200

    Add initial implementation of cat program with file reading and output

---
+++ chap03/cat_v3.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+
+const Io = std.Io;
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+    const arena = init.arena.allocator();
+
+    const args = try init.minimal.args.toSlice(arena);
+    const cwd = Io.Dir.cwd();
+
+    const stdout_file = Io.File.stdout();
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = stdout_file.writer(io, &stdout_buffer);
+
+    const stdout = &stdout_writer.interface;
+
+    for (args[1..]) |filepath| {
+        const text = try cwd.readFileAlloc(io, filepath, init.gpa, .unlimited);
+        defer init.gpa.free(text);
+        try stdout.writeAll(text);
+    }
+
+    try stdout.flush();
+}
